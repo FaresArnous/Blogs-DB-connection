a5d15c43b3428ddf6687dc3558e5fbd60235acf3
fares Arnous <Faris.Arnous@hotmail.com>
2024-10-22 12:56:26 +0300

    change folder location

--- data/database.js
+++ data/database.js
@@ -1,9 +1,13 @@
 const mysql = require("mysql2/promise");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const pool = mysql.createPool({
-  host: "your host",
-  database: "your database",
-  user: "your user",
-  password: "Your password",
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool;
